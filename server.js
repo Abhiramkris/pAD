3708a97f02afb01c3a59a31e8f8dd2a45a73377a
abhiram krishna <kabhiram67@gmail.com>
2025-01-25 01:08:02 +0530

    V

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Schema for Transactions
 const TransactionSchema = new mongoose.Schema({
